9cb63cc9c8445f2c002ae847601a9b9d8e24c04d
Filip Hřivňacký <118018400+FilipStyno@users.noreply.github.com>
2025-05-06 14:35:50 +0200

    Add files via upload

---
+++ team/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('players/', views.player_list, name='player_list'),
+    path('trainers/', views.trainer_list, name='trainer_list'),
+]
--- team/views.py
+++ team/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Player, Trainer
 
-# Create your views here.
+def player_list(request):
+    players = Player.objects.select_related('person').all()
+    return render(request, 'team/player_list.html', {'players': players})
+
+def trainer_list(request):
+    trainers = Trainer.objects.select_related('person').all()
+    return render(request, 'team/trainer_list.html', {'trainers': trainers})
